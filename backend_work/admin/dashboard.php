febaf176a97b19f965ac39b5bdd1321b1f4656d4
cnchaula <92134925+cnchaula@users.noreply.github.com>
2022-09-03 03:16:36 +0700

    BE apakah ini maksudnya???

--- backend_work/admin/dashboard.php
+++ backend_work/admin/dashboard.php
@@ -2,11 +2,38 @@
 <section id="dashboard">
     <h4>ERP STUDY GROUP WEB DASHBOARD</h4>
     <h5>Informasi Menu Website</h5>
-    <p>Home</P>
-    <p>Gallery</P>
-    <p>Info Lomba</P>
-    <p>Kegiatan</P>
-    <p>Contact Us</P>
+    <p><b>Home</b></p>
+    <p>Menu Home merupakan tampilan awal dari website ERP Study Group dan berisikan informasi mengenai ERP Study Group.</p>
+    <ol>
+        <li>Video: Tambah, Edit, Hapus Video</li>
+        <li>Tentang Kami: Tambah, Edit, Hapus deskripsi Tentang Kami</li>
+        <li>Kegiatan: Tambah, Edit, Hapus kegiatan</li>
+        <li>Media Sosial: Tambah, Edit, Hapus media sosial</li>
+    </ol> 
+    <p><b>Gallery</b></P>
+    <p>Menu Gallery berisikan dokumentasi seluruh kegiatan yang diadakan oleh ERP Study Group.</p>
+    <ol>
+        <li>Kategori: Tambah, Edit, Hapus Kategori</li>
+        <li>Foto kegiatan: Tambah, Edit, Hapus foto</li>
+    </ol>
+    <p><b>Info Lomba</b></P>
+    <p>Menu Info Lomba berisikan informasi mengenai lomba-lomba yang sedang berlangsung.</p>
+    <ol>
+        <li>Hall of Fame: Tambah Foto anggota berprestasi</li>
+        <li>Kategori Lomba: Edit kategori</li>
+        <li>Lomba: Tambah, Edit, Hapus lomba</li>
+    </ol>
+    <p><b>Kegiatan</b></P>
+    <p>Menu Kegiatan berisikan informasi mengenai seluruh kegiatan yang diadakan oleh ERP Study Group.</p>
+    <ol>
+        <li>Info Kegiatan: Tambah Foto poster kegiatan</li>
+        <li>Kegiatan: Tambah, Edit, Hapus kegiatan</li>
+    </ol>
+    <p><b>Contact Us</b></P>
+    <p>Menu Contact Us diperuntukkan sebagai media komunikasi antara pengunjung website dengan ERP Study Group.</p>
+    <ol>
+        <li>Feedback: Lihat, Hapus feedback</li>
+    </ol>
 </section>
 
 
